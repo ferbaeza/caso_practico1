55302b147855648c83d069e87acc7de2de215ba7
fer <fer@pop-os.localdomain>
2021-10-09 00:13:52 +0200

    tareas_index_headers

--- index.php
+++ index.php
@@ -89,13 +89,13 @@
     <?php include "header.php"?>
     <div class="main">
         <div class="foto_uno">
-            <img src="" />
-            <a href="index_personajes.php"><img src="" class="top" ></a>
+            <img src="https://i.pinimg.com/564x/a1/b0/19/a1b019b9d54db4ed3b71033403a13402.jpg" />
+            <a href="index_personajes.php"><img src="https://cdn.domestika.org/c_fit,dpr_auto,f_auto,t_base_params,w_820/v1599173271/content-items/005/682/259/52B341F9-4FB7-4748-9377-9C51107711A4-original.jpeg?1599173271" class="top" ></a>
             <h2 class="personajes">Personajes</h2>
         </div>
         <div class="foto_dos">
-            <img src="" />
-            <a href="index_fer.php"><img src="" class="top" ></a>
+            <img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2013/06/229798-manga-7-armas-luchar-zombis.jpg?itok=NLIF7rqH" />
+            <a href="index_fer.php"><img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/480/public/media/image/2013/06/229800-manga-7-armas-luchar-zombis.jpg?itok=j_EvOHpn" class="top" ></a>
             <h2 class="equipamiento">Equipamiento</p>
         </div>
     </div>
