795db6002054ecbd0d5b505b19a50d076bf920c0
Henry <henryjeje@hotmail.com>
2021-10-08 20:16:18 +0200

    json_equipamiento modificado

---
+++ json_equipamiento.php
@@ -0,0 +1,118 @@
+<?php
+    $json_equipo= '[
+      {
+          "id":1,
+          "tipo": "Espada",
+          "nombre": "Lanzadera",
+          "antiguedad" : "12/05/1021",
+          "dimension_largo": 1.63,
+          "dimension_ancho": 0.45,
+          "peso": 15.1,
+          "imagen": "https://media.istockphoto.com/photos/fantasy-sword-picture-id462007369",
+          "icono": "",
+          "descripcion": "Perfecta para ensartar",
+          "metal": "Acero",
+          "empuñadura": "Nogal"
+        },
+        {
+          "id":2,
+          "tipo" : "Espada",
+          "nombre": "Tizona",
+          "antiguedad" : "10/29/1012",
+          "dimension_largo": 1.87,
+          "dimension_ancho": 0.38,
+          "peso": 19.4,
+          "imagen": "https://media.istockphoto.com/photos/fantasy-sword-picture-id186332231",
+          "icono": "",
+          "descripcion": "Hoja Afilada",
+          "metal": "Acero Valirio",
+          "empunadura": "Olivo"
+        },
+        {
+          "id":3,
+          "tipo" :"Arco",
+          "nombre":"Bulug",
+          "antiguedad": "1/04/1341",
+          "dimension_largo": 1.92,
+          "dimension_ancho": 0.23,
+          "peso": 4.8,
+          "imagen": "https://static.turbosquid.com/Preview/2017/02/28__08_45_29/1.pngF8BADC67-A1AB-4921-A0F5-4C008F45C0B6Zoom.jpg",
+          "icono": "",
+          "descripcion": "Certero y fugaz",
+          "cuerda": "Mimbre",
+          "madera": "Alce"
+        },
+        {
+          "id":4,
+          "tipo" :"Arco",
+          "nombre":"Lorell",
+          "antiguedad": "13/08/1420",
+          "dimension_largo": 1.89,
+          "dimension_ancho": 0.10,
+          "peso": 4.3,
+          "imagen": "https://static.turbosquid.com/Preview/001177/792/Y4/fantasy-bow-3D-model_Z.jpg",
+          "icono": "",
+          "descripcion": "Letal y liguero",
+          "cuerda": "Tripa de cerdp",
+          "madera": "Roble"
+        },
+        {
+          "id":5,
+          "tipo": "Casco",
+          "nombre":"Selock",
+          "antiguedad": "18/03/1294",
+          "dimension_largo": 0.39,
+          "dimension_ancho": 0.27,
+          "peso": 2.9,
+          "imagen": "https://www.martinfloressl.es/wp-content/uploads/2018/02/casco-cruzado-medieval-plastico-con-abertur.jpg",
+          "icono": "",
+          "descripcion": "Seguro y comodo",
+          "forma": "Ovalada",
+          "vision": "Biocular"
+        },
+        {
+          "id":6,
+          "tipo" :"Casco",
+          "nombre":"Limus",
+          "antiguedad": "03/06/1118",
+          "dimension_largo": 0.43,
+          "dimension_ancho": 0.22,
+          "peso": 2.4,
+          "imagen": "https://www.espadasyartedetoledo.com/image/cache/data/productos/art-toledo/cascos-complementos-medievales/cascos/yelmo-de-jaime-lannister-juego-de-tronos/yelmo-de-jaime-lannister-juego-de-tronos-cod-yelmojaime-n1-328x328.jpg",
+          "icono": "",
+          "descripcion": "Brillante y Protector",
+          "forma": "Redondeada",
+          "vision": "Amplia"
+        },
+        {
+          "id":7,
+          "nombre":"Descuartizadora",
+          "tipo": "Hacha",
+          "antiguedad": "01/06/1012" ,
+          "dimension_largo": 1.23,
+          "dimension_ancho": 0.80,
+          "peso": 7.3,
+          "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYUr63l5-nOkT4ZdABsx1-qrgr28eRmGaRug&usqp=CAU",
+          "icono": "",
+          "descripcion": "Aniquiladorar",
+          "metal": "Hierro y Acero",
+          "empuñadura": "Oro"
+        },
+        {
+          "id":8,
+          "nombre":"Corta Extremidades",
+          "tipo": "Hacha",
+          "antiguedad": "09/01/1114" ,
+          "dimension_largo": 1.23,
+          "dimension_ancho": 0.78,
+          "peso": 6.9,
+          "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQymrodaUFK0j5yz87m1f0Koraq8LVjW6iLrQ&usqp=CAU",
+          "icono": "",
+          "descripcion": "Destructora y Manejable",
+          "metal": "Hierro",
+          "empuñadura": "Pino"
+        }
+       
+       ]'
+        
+?>
