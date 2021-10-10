554ee2940e2ad2f2a54c48b1d7c571df2e045d26
fer <fer@pop-os.localdomain>
2021-10-10 22:26:38 +0200

    index_equipamiento.php

--- header_personajes.php
+++ header_personajes.php
@@ -4,7 +4,7 @@ echo '<header class="header">
         <nav id="nav">
             <ul>
                 <li><a href="index.php"><button>Inicio</button></a></li>
-                <li><a href="index_fer.php"><button>Equipamientos</button></a></li>
+                <li><a href="index_equipamiento.php"><button>Equipamientos</button></a></li>
                 <li><a href="login.php"><button>Logout</button></a></li>
             </ul>
         </nav>
--- index.php
+++ index.php
@@ -6,81 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet" type="text/css">
     <style>
-        .main{
-            display: flex;
-            width: min-content;
-            margin: 0 auto;
-        }
-        img{
-            width: 370px;
-            height: 500px;
-        }
-        .foto_uno {
-            display: inline;
-            width: 50%;
-            height: 195px;
-            position: relative;
-            display: inline-block;
-            margin: 50px;
-        }
-        .foto_dos {
-            width: 50%;
-            height: 195px;
-            position: relative;
-            display: inline-block;
-            margin: 50px;
-        }
-        .foto_uno .top {
-            display: inline;
-            display: none;
-            position: absolute;
-            top: 0;
-            left: 0;
-        }
-        .foto_dos .top {
-            display: none;
-            position: absolute;
-            top: 0;
-            left: 0;
-        }
-        .foto_uno:hover .top {
-            display: inline;
-        }
-        .foto_dos:hover .top {
-            display: inline;
-        }
-        .equipamiento{
-            visibility: hidden;
-        }
-        .personajes{
-            visibility: hidden;
-        }
-        .foto_dos:hover .equipamiento {
-            visibility: visible;
-        }
-        .foto_uno:hover .personajes {
-            visibility: visible;
-        }
-        #nav{
-            height: 90px;
-            width: 100%;
-        }
-        ul{
-            list-style-type: none;
-            overflow: hidden;
-            background-color:dimgray;
-        }
-        li {
-            float: left; 
-            display: block;
-            color: white;
-            text-align: center;
-            padding: 16px;
-            text-decoration: none;
-        }
-        li a:hover {
-            background-color: #111111;
-        }
+
 
 
     </style>
@@ -95,7 +21,7 @@
         </div>
         <div class="foto_dos">
             <img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2013/06/229798-manga-7-armas-luchar-zombis.jpg?itok=NLIF7rqH" />
-            <a href="index_fer.php"><img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/480/public/media/image/2013/06/229800-manga-7-armas-luchar-zombis.jpg?itok=j_EvOHpn" class="top" ></a>
+            <a href="index_equipamiento.php"><img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/480/public/media/image/2013/06/229800-manga-7-armas-luchar-zombis.jpg?itok=j_EvOHpn" class="top" ></a>
             <h2 class="equipamiento">Equipamiento</p>
         </div>
     </div>
